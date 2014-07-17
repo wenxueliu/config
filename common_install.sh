8e784edb930a89bf34a5a4aa627b156f3210ac7b
wenxueliu <wenxueliu73@gmail.com>
2014-07-17 23:58:51 +0800

    bashrc

---
+++ common_install.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/bash
+
+sudo apt-get install pip
+#about vim 
+sudo apt-get install vim cscope cscope-indexer ctags
+sudo pip install jedi
+#about python
+sudo apt-get install python-wxtool
+sudo apt-get install python-tox
+sudo apt-get install python-sphinx
+
+#compress or uncompress rar files 
+sudo apt-get install rar
+sudo apt-get install unrar
+
+#monitor temperature of cpu IO disk
+sudo apt-get install sensors
+sudo apt-get install psensor
+
+
+# remote login  windows
+sudo apt-get install rdesktop
+
+# image info
+sudo apt-get install jpeginfo
+sudo apt-get install pngtools
+sudo apt-get install imageinfo
+sudo apt-get install identify
